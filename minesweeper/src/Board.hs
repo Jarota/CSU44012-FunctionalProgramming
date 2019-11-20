72d064e99e8c70bcacbeb68fb0dfe0fac57fc79d
Jarota <taitj@tcd.ie>
2019-11-20 22:43:52 +0000

    fully working minesweeper game

--- minesweeper/src/Board.hs
+++ minesweeper/src/Board.hs
@@ -1,71 +1,75 @@
-module Board (Square (..), Board (..), buildBoardString, bombsTouching, makeBoard, setSquare, squareAt) where
+module Board (Square (..), Board (..), buildBoardString, bombsInList, adjI, makeBoard, setSquare, squareAt, checkForWin) where
 
 import System.Random
 import Data.List
 
 data Square = Bomb      -- a bomb is in the square
-            | Covered     -- the square is covered i.e. untouched so far
-            | Flag      -- a flag has been placed in the square
+            | Empty     -- the square is Empty i.e. untouched so far
+            | FlagB     -- a flag has been placed on a bomb
+            | FlagE     -- a flag has been placed on an empty square
             | Clear Int -- the square has been cleared and has Int number of bombs around it
-            deriving Eq
+            deriving (Eq, Show)
 
 -- board dimensions and corresponding list of squares
 data Board = Board (Int, Int) [Square]
 
 buildBoardString :: Board -> String
-buildBoardString (Board (r, c) squares) = buildBoardString' c (r*c) squares
+buildBoardString (Board (r, c) squares) = buildBoardString' c ((r*c)-1) squares
 
 {-
     Takes in the length of a row, the current index, and a list of squares
     Outputs a string
 -}
 buildBoardString' :: Int -> Int -> [Square] -> String
-buildBoardString' _ 0 _ = "\n"
-buildBoardString' c i squares   | i `div` c == 0    = next ++ s ++ "\n"
+buildBoardString' c i squares   | i == 0            = s
+                                | i `mod` c == 0    = next ++ "\n" ++ s
                                 | otherwise         = next ++ s
                                 where
                                     next = buildBoardString' c (i-1) squares
-                                    s = squareString (squares!!i)
+                                    s    = squareString (squares!!i)
+                                    -- s    = show (squares!!i) -- for debugging
 
 squareString :: Square -> String
-squaresString (Clear x)   = show x
-squareString Flag       = "F"
+squareString (Clear x)
+    | x == 0            = " "
+    | otherwise         = show x
+squareString FlagE      = "F"
+squareString FlagB      = "F"
 squareString _          = "."
 
 {-
     Takes in a board and an index
     Outputs the number of bombs touching the square at that index
 -}
-bombsTouching :: Board -> Int -> Int
-bombsTouching (Board (r, c) squares) i = length (filter (\s -> s == Bomb) adjacentSquares)
+bombsInList :: Board -> [Int] -> Int
+bombsInList (Board (r, c) squares) indices = length (filter (\s -> s == Bomb || s == FlagB) adjacentSquares)
         where
-            adjacentIndices = adjI (r, c) i
-            adjacentSquares = adjS squares adjacentIndices
+            adjacentSquares = subsetOfSquares squares indices
 
 adjI :: (Int, Int) -> Int -> [Int]
 adjI (r, c) i   = prevRow ++ left ++ right ++ nextRow
                 where
-                    prevRow = appendRowIf (i - c > 0) c (i-c)
-                    nextRow = appendRowIf (i + c > r * c) c (i+c)
-                    left = appendElemIf (i `div` c == 1) (i-1)
-                    right = appendElemIf (i `div` c == 0) (i+1)
+                    prevRow = appendRowIf (i-c >= 0) c (i-c)
+                    nextRow = appendRowIf (i+c < r*c) c (i+c)
+                    left    = appendElemIf (i `mod` c /= 0) (i-1)
+                    right   = appendElemIf (i `mod` c /= c-1) (i+1)
 
 appendRowIf :: Bool -> Int -> Int -> [Int]
 appendRowIf inBounds c i
     | inBounds  = left ++ [i] ++ right
     | otherwise = []
     where
-        left = appendElemIf (i `div` c == 1) (i-1)
-        right = appendElemIf (i `div` c == 0) (i+1)
+        left  = appendElemIf (i `mod` c /= 0) (i-1)
+        right = appendElemIf (i `mod` c /= c-1) (i+1)
 
 appendElemIf :: Bool -> Int -> [Int]
 appendElemIf inBounds i
     | inBounds  = [i]
     | otherwise = []
 
-adjS :: [Square] -> [Int] -> [Square]
-adjS _ [] = []
-adjS squares (i:is) = (squares!!i):(adjS squares is)
+subsetOfSquares :: [Square] -> [Int] -> [Square]
+subsetOfSquares _ []            = []
+subsetOfSquares squares (i:is)  = (squares!!i):(subsetOfSquares squares is)
 
 
 squareAt :: Board -> Int -> Square
@@ -77,7 +81,7 @@ squareAt (Board dims squares) i = squares!!i
 -}
 setSquare :: Board -> Int -> Square -> Board
 setSquare (Board dims squares) i newSquare =
-        let (ys,zs) = splitAt (i-1) squares in
+        let (ys,zs) = splitAt i squares in
             Board dims (ys ++ [newSquare] ++ tail zs)
 
 {-
@@ -87,16 +91,16 @@ setSquare (Board dims squares) i newSquare =
 makeBoard :: (Int, Int) -> Int -> Board
 makeBoard dims numBombs = Board dims squares
             where
-                bombs = makeBombList dims numBombs
+                bombs   = makeBombList dims numBombs
                 squares = makeSquareList dims bombs
 
 makeSquareList :: (Int, Int) -> [Int] -> [Square]
-makeSquareList (r, c) bombs = makeSquareList' (r*c) bombs
+makeSquareList (r, c) bombs = makeSquareList' ((r*c)-1) bombs
 
 makeSquareList' :: Int -> [Int] -> [Square]
-makeSquareList' 0 _     = []
-makeSquareList' i bombs | i `elem` bombs    = Bomb:(makeSquareList' (i-1) bombs)
-                        | otherwise         = Covered:(makeSquareList' (i-1) bombs)
+makeSquareList' i bombs | i < 0             = []
+                        | i `elem` bombs    = Bomb:(makeSquareList' (i-1) bombs)
+                        | otherwise         = Empty:(makeSquareList' (i-1) bombs)
 
 makeBombList :: (Int, Int) -> Int -> [Int]
 makeBombList (r, c) numBombs = makeBombList' (r*c) numBombs []
@@ -104,8 +108,12 @@ makeBombList (r, c) numBombs = makeBombList' (r*c) numBombs []
 makeBombList' :: Int -> Int -> [Int] -> [Int]
 makeBombList' maxIndex n bombsSoFar
             | numBombs >= n    = take n bombsSoFar
-            | otherwise                 = makeBombList' maxIndex n ( nub (newBombs ++ bombsSoFar) )
+            | otherwise        = makeBombList' maxIndex n ( nub (newBombs ++ bombsSoFar) )
             where
                 numBombs = length bombsSoFar
-                g = mkStdGen numBombs
-                newBombs =  take n (randomRs (1, maxIndex+1) g)
+                g        = mkStdGen numBombs
+                newBombs =  take n (randomRs (0, maxIndex-1) g)
+
+
+checkForWin :: Board -> Bool
+checkForWin (Board _ squares) = (Empty `notElem` squares) && (FlagE `notElem` squares)
--- minesweeper/src/Lib.hs
+++ minesweeper/src/Lib.hs
@@ -23,61 +23,88 @@ play :: GameState -> IO ()
 play (GS _ _ Lose) = putStrLn "You Lose!"
 play (GS _ _ Win) = putStrLn "You Win!"
 
-play (GS board flagsLeft _) = do
-        printGame (GS board flagsLeft Playing)
-        putStrLn "What is your move? f(i,j) / c(i,j)"
-        moveString <- getLine
-        let move = stringToMove board moveString
-        if move == Invalid
-            then do
-                putStrLn "That is not a valid move."
-                play (GS board flagsLeft Playing)
-            else
-                play (makeMove (GS board flagsLeft Playing) move)
+play (GS board flagsLeft _)
+        | checkForWin board = play (GS board flagsLeft Win)
+        | otherwise         = do
+                    printGame (GS board flagsLeft Playing)
+                    putStrLn "What is your move? f(i,j) / c(i,j)"
+                    moveString <- getLine
+                    let move = stringToMove board moveString
+                    if move == Invalid
+                        then do
+                            putStrLn "That is not a valid move."
+                            play (GS board flagsLeft Playing)
+                        else
+                            play (makeMove (GS board flagsLeft Playing) move)
 
 printGame :: GameState -> IO ()
 printGame (GS board flagsLeft _) = putStrLn gameString
         where
             boardString = buildBoardString board
-            gameString = "You have " ++ (show flagsLeft) ++ " flags left.\n" ++ boardString
+            gameString  = "You have " ++ (show flagsLeft) ++ " flags left.\n" ++ boardString
 
 
 stringToMove :: Board -> String -> Move
 stringToMove (Board (r, c) _) moveString
         | (length moveString) /= 6  = Invalid
-        | index == 0                = Invalid
+        | index == (r*c)            = Invalid
         | action == 'f'             = FlagSqaure index
         | action == 'c'             = ClearSquare index
         | otherwise                 = Invalid
         where
-            action = head moveString
-            i = digitToInt (moveString!!2)
-            j = digitToInt (moveString!!4)
-            index = validIndex (r, c) (i, j)
+            action  = head moveString
+            i       = digitToInt (moveString!!2)
+            j       = digitToInt (moveString!!4)
+            index   = validIndex (r, c) (i, j)
 
 validIndex :: (Int, Int) -> (Int, Int) -> Int
 validIndex (r, c) (i, j)
-        | i <= r && i > 0 && j <= c && j > 0    = (i*r) + j
-        | otherwise                             = 0
+        | i < r && i >= 0 && j < c && j >= 0  = (i*c) + j
+        | otherwise                           = r*c
 
 makeMove :: GameState -> Move -> GameState
 makeMove game (ClearSquare i) = clearSquare game i
 makeMove game (FlagSqaure i)  = flagSquare game i
 
 clearSquare :: GameState -> Int -> GameState
-clearSquare (GS board flagsLeft _) i
-        | prevSquare == Bomb      = GS board flagsLeft Lose
-        | prevSquare == Covered   = GS (setSquare board i newSquare) flagsLeft Playing
-        | otherwise         = GS board flagsLeft Playing
+clearSquare (GS (Board (r, c) squares) flagsLeft _) i
+        | prevSquare == Bomb        = GS board flagsLeft Lose
+        | alreadyClear              = passGameState
+        | prevSquare == FlagB       = passGameState
+        | prevSquare == FlagE       = passGameState
+        | adjBombs == 0             = clearAdjSquares newGameState adjIndices
+        | otherwise                 = newGameState
         where
-            prevSquare = squareAt board i
-            newSquare = Clear (bombsTouching board i)
+            board           = Board (r, c) squares
+            prevSquare      = squareAt board i
+            passGameState   = GS board flagsLeft Playing
+            alreadyClear    = any (prevSquare==) [(Clear 0), (Clear 1), (Clear 2), (Clear 3), (Clear 4), (Clear 5), (Clear 6), (Clear 7), (Clear 8)]
+            adjIndices      = adjI (r, c) i
+            adjBombs        = bombsInList board adjIndices
+            newSquare       = Clear adjBombs
+            newGameState    = GS (setSquare board i newSquare) flagsLeft Playing
+
+clearAdjSquares :: GameState -> [Int] -> GameState
+clearAdjSquares game []     = game
+clearAdjSquares game (i:is) = clearAdjSquares (newGameState) is
+        where
+            newGameState = clearSquare game i
+
+
 
 flagSquare :: GameState -> Int -> GameState
+flagSquare (GS board 0 _) i
+        | prevSquare == FlagB   = GS (setSquare board i Bomb) 1 Playing
+        | prevSquare == FlagE   = GS (setSquare board i Empty) 1 Playing
+        | otherwise             = GS board 0 Playing
+        where
+            prevSquare = squareAt board i
+
 flagSquare (GS board flagsLeft _) i
-        | prevSquare == Bomb    = newGameState
-        | prevSquare == Covered = newGameState
+        | prevSquare == Bomb    = GS (setSquare board i FlagB) (flagsLeft-1) Playing
+        | prevSquare == Empty   = GS (setSquare board i FlagE) (flagsLeft-1) Playing
+        | prevSquare == FlagB   = GS (setSquare board i Bomb) (flagsLeft+1) Playing
+        | prevSquare == FlagE   = GS (setSquare board i Empty) (flagsLeft+1) Playing
         | otherwise             = GS board flagsLeft Playing
         where
             prevSquare = squareAt board i
-            newGameState = GS (setSquare board i Flag) (flagsLeft-1) Playing
